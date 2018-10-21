0a640937f00f85c7ee89441df834d27f1c6882e0
JayDoubleU1003 <44334025+JayDoubleU1003@users.noreply.github.com>
2018-10-21 15:08:10 +0800

    Update Project idea.py

--- Project idea.py
+++ Project idea.py
@@ -1,14 +1,14 @@
-request name, request height, request weight, calculate BMI, read from file to find out which range name is in. 
-records down name and bmi value (write), which allows for further referring and monitoring.  
-request for daily practice, read from file, ask if wants suggestion, if yes, give suggestion. 
+#request name, request height, request weight, calculate BMI, read from file to find out which range name is in. 
+#records down name and bmi value (write), which allows for further referring and monitoring.  
+#request for daily practice, read from file, ask if wants suggestion, if yes, give suggestion. 
 
-request blood pressure and help records for monitoring. (as well as date, write) (daily steps maybe)
-and to maybe find out when is bp high 
+#request blood pressure and help records for monitoring. (as well as date, write) (daily steps maybe)
+#and to maybe find out when is bp high 
 
         
             
 
-make it compatible with phone (challenge)  
+#make it compatible with phone (challenge)  
 
 
 
