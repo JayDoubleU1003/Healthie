2b8419d88560de3c87bd72b18d2ea9bcd8fa75f5
JayDoubleU1003 <44334025+JayDoubleU1003@users.noreply.github.com>
2018-10-26 23:04:12 +0800

    Update Login_Module.py

--- Login_Module.py
+++ Login_Module.py
@@ -3,7 +3,9 @@ import os
 import Processes_Module
 
 creds = "tempfile.temp" 
- 
+HEADING = "TimesNewRoman 12 bold"
+SIZE = "400x400"
+
 def Signup(): # This is the signup definition, 
     global pwordE # These globals just make the variables global to the entire script, meaning any definition can use them
     global nameE
@@ -11,8 +13,9 @@ def Signup(): # This is the signup definition,
  
     rootsignup = Tk() # This creates the window, just a blank one.
     rootsignup.title("Sign Up") # This renames the title of said window to 'signup'
-    intruction = Label(rootsignup, text="Please Enter new Username and password\n") # This puts a label, so just a piece of text saying 'please enter blah'
-    intruction.grid(row=0, columnspan=3, sticky=E) # This just puts it in the window, on row 0, col 0. If you want to learn more look up a tkinter tutorial :)
+    rootsignup.geometry(SIZE)
+    intruction = Label(rootsignup, text="Please Enter new Username and password\n", font = HEADING) # This puts a label, so just a piece of text saying 'please enter blah'
+    intruction.grid(row=0, columnspan=5,) # This just puts it in the window, on row 0, col 0. If you want to learn more look up a tkinter tutorial :)
  
     nameL = Label(rootsignup, text="New Username: ") # This just does the same as above, instead with the text new username.
     pwordL = Label(rootsignup, text="New Password: ") # ^^
@@ -21,8 +24,8 @@ def Signup(): # This is the signup definition,
  
     nameE = Entry(rootsignup) # This now puts a text box waiting for input.
     pwordE = Entry(rootsignup, show="*") # Same as above, yet 'show="*"' What this does is replace the text with *, like a password box :D
-    nameE.grid(row=1, column=1) # You know what this does now :D
-    pwordE.grid(row=2, column=1) # ^^
+    nameE.grid(row=1, column=1, sticky = W) # You know what this does now :D
+    pwordE.grid(row=2, column=1, sticky = W) # ^^
  
     signupButton = Button(rootsignup, text="Sign up", command=FSSignup) # This creates the button with the text 'signup', when you click it, the command 'fssignup' will run. which is the def
     signupButton.grid(columnspan=2, sticky=W)
@@ -43,7 +46,7 @@ def FSSignup():
       signupagain = Tk() #creates a new window when sign up informations are not provided. 
       signupagain.title("Signup failed")
       signup_fail = Label(signupagain, text = "Please fill in required info\n", fg = "red")
-      signupagain.geometry("250x75")
+      signupagain.geometry(SIZE)
       signup_fail.pack()
       
       signupa_B = Button(signupagain, text = "Sign up again", command = signupagain.destroy)
@@ -54,12 +57,12 @@ def FSSignup():
  
 def Login():
     global nameEL
-    global pwordEL # More globals :D
+    global pwordEL 
     global rootA
  
     rootA = Tk() # This now makes a new window.
     rootA.title("Login") # This makes the window title 'login'
- 
+    rootA.geometry(SIZE)
     intruction = Label(rootA, text="Please Login\n") # More labels to tell us what they do
     intruction.grid(sticky=E) # Blahdy Blah
  
@@ -81,6 +84,8 @@ def Login():
     rootA.mainloop()
  
 def CheckLogin():
+    global r
+    
     with open(creds) as f:
         data = f.readlines() # This takes the entire document we put the info into and puts it into the data variable
         uname = data[0].rstrip() # Data[0], 0 is the first line, 1 is the second and so on.
@@ -89,17 +94,17 @@ def CheckLogin():
     if nameEL.get() == uname and pwordEL.get() == pword: # Checks to see if you entered the correct data.
         r = Tk() # Opens new window
         r.title("XD")
-        r.geometry("200x75") # Makes the window a certain size
+        r.geometry(SIZE) # Makes the window a certain size
         rlbl = Label(r, text = "\n[+] Logged In") # "logged in" label
         rlbl.pack()
         continue_Button = Button(r, text = "Continue into app", command = Processes_Module.Main_window) 
         continue_Button.pack()
-
         r.mainloop()
+        
     else:
         r = Tk()
         r.title("D:")
-        r.geometry("150x50")
+        r.geometry(SIZE)
         rlbl = Label(r, text="\n[!] Invalid Login")
         rlbl.pack()
         r.mainloop()
