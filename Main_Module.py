a9a3bcb7d1320ec1a8b289943db1b40d7034f441
JayDoubleU1003 <44334025+JayDoubleU1003@users.noreply.github.com>
2018-10-26 12:35:35 +0800

    Update Main_Module.py

--- Main_Module.py
+++ Main_Module.py
@@ -1,6 +1,15 @@
-import Processes_Module  #Imports the module for usage in this file
+import Processes_Module #Imports the module for usage in this file
 import User_Input_Module #Imports the module for usage in this file 
+import Login_Module
 
-Processes_Module.Main_window()
+
+
+if Login_Module.os.path.isfile(Login_Module.creds):
+    Login_Module.Login()
+else: # This if else statement checks to see if the file exists. If it does it will go to Login, if not it will go to Signup :)
+    Login_Module.Signup()
+
+
+#Processes_Module.Main_window()
 
 
