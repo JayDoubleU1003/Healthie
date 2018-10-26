34ab64857d309c9a852f1c364d41a26ca124b304
ZeeHatcher <zee.hatcher010@gmail.com>
2018-10-26 22:34:49 +0800

    Fixed 'Continue Into App' bug

--- Processes_Module.py
+++ Processes_Module.py
@@ -10,7 +10,7 @@ def BMI_Calculator (height, mass):
 
 def Defining_Health_Range (BMI):
     fileX = open(r"C:\Users\Jw Lim\Python\Swinburne\BMI.txt", "r")
-    
+
     if BMI < 18.5:
         print(fileX.readlines()[1])
     elif BMI < 25:
@@ -23,19 +23,18 @@ def Defining_Health_Range (BMI):
         print(fileX.readlines()[9])
     elif BMI >= 40:
         print(fileX.readlines()[11])
-    
+
     fileX.close
-    
+
 def Main_window():
-    r.destroy()
-    print("Health Monitor") 
+    print("Health Monitor")
     x = input("Press 'Enter' to start recording body mass index, enter 'x' to end program: ")
 
-    if x == "": 
+    if x == "":
         height = float(input("Enter your height in meters: "))
         mass = float(input("Enter your mass in Kg: "))
         BMI_Calculator(height, mass)
         Defining_Health_Range(BMI) #calls Defining_Health_Range function from Processes_Module
 
-    elif x.lower() == "x": 
+    elif x.lower() == "x":
         print("Alright. This program ends here")
