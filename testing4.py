450df82d85eb7346678e13a61f33f8e3473f2592
ZeeHatcher <zee.hatcher010@gmail.com>
2018-11-14 23:09:04 +0800

    fixed error

--- testing4.py
+++ testing4.py
@@ -1,37 +1,37 @@
-from Processes_Module import * 
+from Processes_Module import *
 from Login_New import *
 from tkinter import *
 import os
 import datetime
-#import User_Input_Module 
+#import User_Input_Module
 
-usernamelist = r"usernamelist.txt" #"tempfile.temp"  
+usernamelist = r"usernamelist.txt" #"tempfile.temp"
 HEADING = "TimesNewRoman 14 bold"
 SMALL = "150x150+450+250"
 NORMAL = "500x400+450+250"
 
-def Signup():  
-    global pwordE 
+def Signup():
+    global pwordE
     global nameE
     global rootsignup
- 
-    rootsignup = Tk() 
-    rootsignup.title("Sign Up") 
+
+    rootsignup = Tk()
+    rootsignup.title("Sign Up")
     rootsignup.geometry(NORMAL)
-    intruction = Label(rootsignup, text="Please Enter new Username and password\n", font = HEADING) 
-    intruction.place(x=30, y=50) 
- 
-    nameL = Label(rootsignup, text="New Username: ") 
-    pwordL = Label(rootsignup, text="New Password: ") 
-    nameL.place(x=70, y=100) 
-    pwordL.place(x=73, y=125) 
- 
-    nameE = Entry(rootsignup) 
-    pwordE = Entry(rootsignup, show="*") 
-    nameE.place(x=158, y=101) 
-    pwordE.place(x=158, y=126) 
- 
-    signupButton = Button(rootsignup, text="Sign up", command=check_username) 
+    intruction = Label(rootsignup, text="Please Enter new Username and password\n", font = HEADING)
+    intruction.place(x=30, y=50)
+
+    nameL = Label(rootsignup, text="New Username: ")
+    pwordL = Label(rootsignup, text="New Password: ")
+    nameL.place(x=70, y=100)
+    pwordL.place(x=73, y=125)
+
+    nameE = Entry(rootsignup)
+    pwordE = Entry(rootsignup, show="*")
+    nameE.place(x=158, y=101)
+    pwordE.place(x=158, y=126)
+
+    signupButton = Button(rootsignup, text="Sign up", command=check_username)
     signupButton.place(x=158, y=160)
     rootsignup.mainloop()
 
@@ -48,35 +48,35 @@ def check_username():
             error.title("Signup failed")
             usernametakenL = Label(error, text="Username has been taken\n", fg="red")
             usernametakenL.pack()
-                
+
             signupa_B = Button(error, text="Sign up again", command=error.destroy)
             signupa_B.pack()
-                
+
             break
         else:
             continue
-            
+
     if newusername == "" or newpassword == "":
-        signupagain = Tk() #creates a new window when sign up informations are not provided. 
+        signupagain = Tk() #creates a new window when sign up informations are not provided.
         signupagain.title("Signup failed")
         signup_fail = Label(signupagain, text="Please fill in required info\n", fg="red")
         signupagain.geometry(SMALL)
         signup_fail.pack()
-                    
+
         signupa_B = Button(signupagain, text="Sign up again", command=signupagain.destroy)
         signupa_B.pack()
-                    
+
         signupagain.mainloop()
-    
+
     elif create:
         add_new_user(newusername, newpassword)
         usersavefile = format_text(newusername)
         create_save_file(usersavefile)
-        rootsignup.destroy() 
-        Login() 
-    
-        
-    
+        rootsignup.destroy()
+        Login()
+
+
+
 #    except:
 #        newusername = nameE.get()
 #        create_file(usernamelist)
@@ -84,43 +84,43 @@ def check_username():
 #        add_new_user(nameE.get(), pwordE.get())
 #        usersavefile = format_text(newusername)
 #        create_save_file(usersavefile)
-    
+
 
 def Login():
     global nameEL
-    global pwordEL 
+    global pwordEL
     global rootA
 
-    rootA = Tk() 
-    
+    rootA = Tk()
 
-    rootA.title("Login") 
+
+    rootA.title("Login")
     rootA.geometry(NORMAL)
-    intruction = Label(rootA, text="Please Login\n", font=HEADING) 
+    intruction = Label(rootA, text="Please Login\n", font=HEADING)
     intruction.pack(side=TOP)
     frame1 = Frame(rootA)
-    frame1.pack() 
- 
-    nameL = Label(frame1, text="Username: ") 
-    pwordL = Label(frame1, text="Password: ") 
+    frame1.pack()
+
+    nameL = Label(frame1, text="Username: ")
+    pwordL = Label(frame1, text="Password: ")
     nameL.grid(row=0, column=0, sticky=E)
     pwordL.grid(row=1, column=0, sticky=E)
- 
-    nameEL = Entry(frame1) 
+
+    nameEL = Entry(frame1)
     pwordEL = Entry(frame1, show="*")
     nameEL.grid(row=0, column=1, sticky=W)
     pwordEL.grid(row=1, column=1, sticky=W)
-    
-    loginB = Button(rootA, text="Login", command=to_CheckLogin) 
+
+    loginB = Button(rootA, text="Login", command=to_CheckLogin)
     loginB.pack()
- 
+
     newsignup = Button(rootA, text="Create new user account", command=New_Signup)
     newsignup.pack()
-    
-#    rmuser = Button(rootA, text = "Delete User", fg = "red")#, command = DelUser) 
+
+#    rmuser = Button(rootA, text = "Delete User", fg = "red")#, command = DelUser)
 #    rmuser.grid(row=5, columnspan=2, sticky=W)
-    
-    
+
+
     rootA.mainloop()
 
 def to_CheckLogin():
@@ -135,20 +135,20 @@ def CheckLogin(nameEL, pwordEL):
     global r
 
     authorized = login(nameEL, pwordEL)
-    username = nameEL  
+    username = nameEL
 
     if authorized:
         to_application(username)
-        
-#            r = Tk() 
+
+#            r = Tk()
 #            r.title("XD")
-#            r.geometry(SMALL) 
-#            rlbl = Label(r, text = "\n[+] Logged In") 
+#            r.geometry(SMALL)
+#            rlbl = Label(r, text = "\n[+] Logged In")
 #            rlbl.pack()
-#            continue_Button = Button(r, text = "Continue into app", command = to_application)#, command = Processes_Module.Main_window) 
+#            continue_Button = Button(r, text = "Continue into app", command = to_application)#, command = Processes_Module.Main_window)
 #            continue_Button.pack()
 #            r.mainloop()
-        
+
     else:
         rootA.destroy()
         r = Tk()
@@ -174,39 +174,39 @@ def Application(username):
     global Homepage
     global Bpage
     global Rpage
-    
-    Application = Tk()
-    Application.withdraw()
+
+    App = Tk()
+    App.withdraw()
     user = username
 
-    Homepage = Toplevel(Application)
+    Homepage = Toplevel(App)
 
     Homepage.title("Healthie")
     Homepage.geometry(NORMAL)
-    
+
     welcome_text = "Welcome \"" + user + "\""
 
     heading1 = Label(Homepage, text=welcome_text, font=HEADING)
     heading1.pack()
-    
+
     Hcontainer = Frame(Homepage)
     Hcontainer.pack()
-    
+
     BmiButton = Button(Hcontainer, text="Begin inputting data for BMI and blood pressure", command=to_BMI_Page)
     BmiButton.grid()
     RecordsButton = Button(Hcontainer, text="Previous records", command=to_Records_Page)
     RecordsButton.grid()
 
 ########################################################################################################################
-    
-    Bpage = Toplevel(Application) 
+
+    Bpage = Toplevel(App)
 
     Bpage.title("Healthie")
     Bpage.geometry(NORMAL)
-    
+
     heading = Label(Bpage, text="BMI & Blood pressure", font=HEADING)
     heading.pack()
-        
+
     Bcontainer = Frame(Bpage)
     Bcontainer.pack()
 
@@ -228,7 +228,7 @@ def Application(username):
     dbp_entry = Entry(Bcontainer)
     dbp_entry.grid(row=3, column=1)
 
-    calculate_button = Button(Bcontainer, text="Calculate now", command=calc_n_show_BMI)
+    calculate_button = Button(Bcontainer, text="Calculate now", command=lambda: calc_n_show_BMI(heightentry, massentry, result))
     calculate_button.grid(row=4, column=0)
 
     Save_Button = Button(Bcontainer, text="Save results")#, command=)
@@ -243,14 +243,14 @@ def Application(username):
     result = StringVar()
     resultL = Label(Bcontainer, textvariable = result)
     resultL.grid(row=5, column=0)
-    
+
 ########################################################################################################################
 
-    Rpage = Toplevel(Application)
+    Rpage = Toplevel(App)
 
     Rpage.title("Healthie")
     Rpage.geometry(NORMAL)
-    
+
     heading = Label(Rpage, text="Your previous records", font=HEADING)
     heading.pack()
 
@@ -269,22 +269,21 @@ def Application(username):
     MenuButton = Button(Rcontainer, text="Back to menu", command=to_Homepage)
     MenuButton.pack()
 
+    App.mainloop()
 
-    Application.mainloop()
-
-def calc_n_show_BMI():
+def calc_n_show_BMI(heightentry, massentry, result):
     h = heightentry.get()
     m = massentry.get()
     BMI = BMI_Calculator(h, m)
-    text ="Your BMI is " + BMI + "" + Defining_Health_Range(BMI)
-    result.set(text)    
-   
-def record_data():   
+    text ="Your BMI is " + str(BMI) + "" + Defining_Health_Range(BMI)
+    result.set(text)
+
+def record_data():
     date = datetime.date.today().strftime("%d %b %Y")
     username = nameEL.get()
     filename = format_text(username)
     write_records(filename, BMI, date)
-        
+
 def to_Homepage():
     Homepage.lift()
 
@@ -295,7 +294,7 @@ def to_Records_Page():
     Rpage.lift()
 
 
-    
+
 #        height = heightentry.get()
 #        mass = massentry.get()
 #        BMI = BMI_Calculator(height, mass)
@@ -304,41 +303,40 @@ def to_Records_Page():
 #        savefile = format_text(username)
 #        write_records(username, BMI, date)
 
-        
-
-
-    
 
 
 
 
 
-    
 
 
 
-        
+
+
+
+
+
+
 
 #    def read_and_display(self):
 #        username = nameEL.get()
 #        filename = format_text(username)
-#        self.message_var.set(read_records)  
+#        self.message_var.set(read_records)
 
 
 
 
 
-        
 
 
-        
+
+
 
 
 
 try:
     check_file(usernamelist)
-    Login()   
+    Login()
 except:
     create_file(usernamelist)
     Signup()
-
