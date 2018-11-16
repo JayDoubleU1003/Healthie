9c9bfb394652a7f50fca9afbe99e8652d0e97b66
JayDoubleU1003 <44334025+JayDoubleU1003@users.noreply.github.com>
2018-11-16 16:14:45 +0800

    Update Processes_Module.py

--- Processes_Module.py
+++ Processes_Module.py
@@ -2,32 +2,36 @@
 import webbrowser
 
 def BMI_Calculator(height, mass):
-    calculation = float(mass) / ( float(height) * float(height) ) * 10000
-    print(calculation)
-    BMI = float("{:0.2f}".format(calculation))
+    calculation = (float(mass)/(float(height)*float(height)))
+    BMI = float("{:.1f}".format(calculation))
     return BMI
 
 def Defining_Health_Range(BMI):
     fileX = open("BMI.txt", "r")
+    
     text = ""
 
     if BMI < 18.5:
         text = (fileX.readlines()[1])
+        return text
     elif BMI < 25:
         text = (fileX.readlines()[3])
+        return text
     elif BMI < 30:
         text = (fileX.readlines()[5])
+        return text
     elif BMI < 35:
         text = (fileX.readlines()[7])
+        return text
     elif BMI < 40:
         text = (fileX.readlines()[9])
+        return text
     elif BMI >= 40:
         text = (fileX.readlines()[11])
-
-    fileX.close()
-
-    return text
-
+        return text
+    
+    fileX.close
+    
 def search_online():
     q = input("What do you want?: ")
     search = "http://www.google.com/search?hl=en&q=" + q
@@ -38,7 +42,7 @@ def read_records(filename):
         text = records.read()
         return text
 
-def write_records(filename, BMI, Date):
-    record_text = "\n" + str(BMI) + "\t" + Date
+def write_records(filename, BMI, Date, SBP, DBP):
+    record_text = "\n|\t" + str(BMI) + "\t\t||\t\t(" + str(SBP) + ",\t" + str(DBP) + ")\t||\t" + Date + "\t|"
     with open(filename, "a") as f:
         f.write(record_text)
