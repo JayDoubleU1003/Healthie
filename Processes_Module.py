b49b0b40e12b7b7f583097186633bb678d352f23
JayDoubleU1003 <44334025+JayDoubleU1003@users.noreply.github.com>
2018-11-23 23:46:06 +0800

    Update Processes_Module.py

--- Processes_Module.py
+++ Processes_Module.py
@@ -1,12 +1,11 @@
-#import Login_New
 import webbrowser
 
-def BMI_Calculator(height, mass):
+def BMI_Calculator(height, mass):   #calculates BMI
     calculation = (float(mass)/(float(height)*float(height)))
     BMI = float("{:.1f}".format(calculation))
     return BMI
 
-def Defining_Health_Range(BMI):
+def Defining_Health_Range(BMI):     #gives feedback on BMI condition based on pre-written text file
     fileX = open("BMI.txt", "r")
     
     text = ""
@@ -32,17 +31,25 @@ def Defining_Health_Range(BMI):
     
     fileX.close
     
-def search_online():
-    q = input("What do you want?: ")
+def search_online(BMI):     #opens default browser and searches on solution for overweight or underweight solution
+    q = ""
+
+    if BMI <= 18.5:
+        q = "Underweight solution" 
+    elif BMI >= 25:
+        q = "Overweight solution"
+    
+
     search = "http://www.google.com/search?hl=en&q=" + q
     webbrowser.open(search)
 
-def read_records(filename):
+def read_records(filename): 
     with open(filename, "r") as records:
         text = records.read()
         return text
 
-def write_records(filename, BMI, Date, SBP, DBP):
-    record_text = "\n|\t" + str(BMI) + "\t\t||\t\t(" + str(SBP) + ",\t" + str(DBP) + ")\t||\t" + Date + "\t|"
+def write_records(filename, BMI, Date, SBP, DBP):   #formats BMI and blood pressure results and writes into save file
+
+    record_text = "\n| " + str(BMI) + "\t|| \t(" + str(SBP) + ",\t" + str(DBP) + ")\t\t|| " + Date + " |"
     with open(filename, "a") as f:
         f.write(record_text)
